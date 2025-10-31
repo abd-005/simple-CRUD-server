867bb0276c07d9c8aa13f4a15d3c5ac76af35c38
abd-005 <abd.me7@gmail.com>
2025-10-31 22:21:19 +0600

    MongoDB User created

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -7,6 +8,19 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+// simpleDBUser
+// q8UtWfvLMVzQawfA
+
+const uri = "mongodb+srv://simpleDBUser:q8UtWfvLMVzQawfA@cluster0.ljdyez8.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
 app.get('/',(req,res)=>{
     res.send('CRUD server is Running');
@@ -15,3 +29,10 @@ app.get('/',(req,res)=>{
 app.listen(port,()=>{
     console.log(`CRUD server is running on: ${port}`)
 })
+
+
+/*
+ * 1. at least one user
+ * 2. set uri with userId and password
+ * 3. create a mongodb client
+*/
